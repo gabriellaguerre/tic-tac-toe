93f0c64421b83bcd4bf4aa65081331de238230cd
gabriellaguerre <laguerre.g.g@gmail.com>
2022-12-22 11:31:26 -0500

    start game button

---
+++ tic-tac-toe.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const a0 = document.getElementById("a0");
+    const a1 = document.getElementById("a1");
+    const a2 = document.getElementById("a2");
+    const a3 = document.getElementById("a3");
+    const a4 = document.getElementById("a4");
+    const a5 = document.getElementById("a5");
+    const a6 = document.getElementById("a6");
+    const a7 = document.getElementById("a7");
+    const a8 = document.getElementById("a8");
+
+    const button = document.getElementById("startGame")
+    button.addEventListener("click", () => {
+        a0.innerText = '';
+        a1.innerText = '';
+        a2.innerText = '';
+        a3.innerText = '';
+        a4.innerText = '';
+        a5.innerText = '';
+        a6.innerText = '';
+        a7.innerText = '';
+        a8.innerText = '';
+    })
+
+})
